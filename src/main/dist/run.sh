1fd8a482497f7fe6721b5c396c9cc7389f8872b4
llamersmcw <llamers@mcw.edu>
2025-05-27 11:52:37 -0500

    Update run.sh

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -6,6 +6,8 @@
 APPNAME=hrdp-variant-load-pipeline
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 
+EMAILLIST="mtutaj@mcw.edu llamers@mcw.edu motutaj@mcw.edu"
+
 APPDIR=/home/rgddata/pipelines/$APPNAME
 cd $APPDIR
 
@@ -13,4 +15,4 @@ java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
     -Xmx20g -jar lib/$APPNAME.jar --mapKey 372 "$@" > run.log 2>&1
 
-mailx -s "[$SERVER] HRDP Variant load Pipeline Run" mtutaj@mcw.edu,llamers@mcw.edu,motutaj@mcw.edu < $APPDIR/logs/summary.log
+mailx -s "[$SERVER] HRDP Variant load Pipeline Run" $EMAILLIST < $APPDIR/logs/summary.log
